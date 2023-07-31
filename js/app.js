9ba367c63c36d21789e4ebbd2685150784263a36
Shimels Alem <shimelsalem@gmail.com>
2023-07-31 11:15:38 +0300

    Adding mobile menu js interaction.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// navigation section
+function openNav() {
+  document.getElementById("overlay").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("overlay").style.width = "0%";
+}
+
+const overlayContainer = document.getElementById("overlay");
+const menuIcon = document.getElementById("menu-icon");
+const mobileMenu = document.getElementsByClassName("mobile-menu");
+const mobNavClosebtn = document.getElementsByClassName("mob-closebtn");
+
+menuIcon.addEventListener("click", (event) => {
+  openNav();
+});
+
+mobNavClosebtn.addEventListener("click", (event) => {
+  closeNav();
+});
